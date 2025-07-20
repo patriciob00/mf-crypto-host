2b7471a9b97368d18f7881f9bc8ff22a712c5cd4
Patricio Bruno Da Costa <pbrunorb@hotmail.com>
2025-07-20 05:36:04 -0300

    types

--- @mf-types/remote_crypto_app/compiled-types/components/crypto-price-chart/index.d.ts
+++ @mf-types/remote_crypto_app/compiled-types/components/crypto-price-chart/index.d.ts
@@ -5,5 +5,5 @@ type Props = {
         price: number;
     }[];
 };
-declare const CryptoPriceChart: ({ data }: Props) => "" | import("react/jsx-runtime").JSX.Element;
+declare const CryptoPriceChart: ({ data }: Props) => import("react/jsx-runtime").JSX.Element;
 export default CryptoPriceChart;
